eda40d9effd303e12459355d0e1ed042219ae609
Artur Walaszczyk <117600329+arturvv100@users.noreply.github.com>
2024-01-13 17:24:06 +0100

    commit

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,7 +6,7 @@
   ******************************************************************************
   */
 
-#include "main.h"
+#include "stm32f4xx_hal.h"
 
 uint16_t map(uint16_t x, uint16_t in_min, uint16_t in_max, uint16_t out_min, uint16_t out_max)
 {
