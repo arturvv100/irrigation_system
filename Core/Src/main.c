46973699510db37302b02ad5fac830d15bc1774c
Artur Walaszczyk <117600329+arturvv100@users.noreply.github.com>
2024-01-13 17:22:34 +0100

    commit

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,17 +1,7 @@
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
+  * Program measure soil moisture using sensor and control water pump to irrigate soil
   *
   ******************************************************************************
   */
